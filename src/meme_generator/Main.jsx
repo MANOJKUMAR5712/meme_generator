fac85fac7378a5726b252bdc2d2eeef49c47a001
Manojkumar K <144757708+MANOJKUMAR5712@users.noreply.github.com>
2024-08-05 15:06:53 +0530

    sample pull

--- src/meme_generator/Main.jsx
+++ src/meme_generator/Main.jsx
@@ -4,6 +4,7 @@ import memes_data from "./memes_data.jsx";
 function Main(){
     let [memeimg,setImage] = useState("https://im.indiatimes.in/facebook/2014/Sep/modi-kfc-meme_1411510375_1411510395.jpg?w=1200&h=900&cc=1&webp=1&q=75");
     let [text,setText] = useState({top_text:"hamara desh mein" , bottom_text:"kuch kuch hota hai"});
+    //sample//
 
     function randomimg(){
         const meme = memes_data.data.memes
