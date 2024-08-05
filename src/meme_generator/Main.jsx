493a6429d5198ec0cffb5e5bc02b17bb051dc60c
MANOJKUMAR5712 <kmanojkumar1098@gmail.com>
2024-08-05 15:02:11 +0530

    feat : added onChange attribute to input tag and added a handletext function to handle input text

--- src/meme_generator/Main.jsx
+++ src/meme_generator/Main.jsx
@@ -3,21 +3,23 @@ import memes_data from "./memes_data.jsx";
 
 function Main(){
     let [memeimg,setImage] = useState("https://im.indiatimes.in/facebook/2014/Sep/modi-kfc-meme_1411510375_1411510395.jpg?w=1200&h=900&cc=1&webp=1&q=75");
-    let [toptext,setTop] = useState("hamara desh mein");
-    let [bottomtext,setBottom] = useState("kuch kuch hota hai");
+    let [text,setText] = useState({top_text:"hamara desh mein" , bottom_text:"kuch kuch hota hai"});
 
     function randomimg(){
         const meme = memes_data.data.memes
         const random_index = Math.floor(Math.random() * meme.length);
-        console.log(random_index);
         const meme_display = meme[random_index].url;
-        console.log(meme_display);
         return meme_display;
     }
 
+    function handleText(event){
+        console.log(event.target.value);
+        setText(prevtext => {
+            return {...prevtext,[event.target.id] : `${event.target.value}` }
+        })
+    }
+
     function handleImage(){
-        setTop(prevtext => prevtext = document.getElementById("top_text").value);
-        setBottom(prevtext => prevtext = document.getElementById("bottom_text").value);
         setImage(randomimg());
     }
     
@@ -25,17 +27,17 @@ function Main(){
         <div id="Main">
             <form action="onclick" className="form">
             <div id="inp_div">
-            <input type="text" id="top_text" placeholder="top text"></input>
-            <input type="text" id="bottom_text" placeholder="bottom text"></input>
+            <input type="text" id="top_text" placeholder="top text" name="top_text" onChange={handleText}></input>
+            <input type="text" id="bottom_text" placeholder="bottom text" name="bottom_text" onChange={handleText}></input>
             </div>
             </form>
             <div id="submit_btn" >
             <button type="submit" id="get_meme" onClick={handleImage}>Get new meme 🖼️</button>
             </div>
             <div id="img">
-                <h2 id="top">{toptext}</h2>
+                <h2 id="top">{text.top_text}</h2>
                 <img src={memeimg} id="meme_img" width="800px" height="400px"></img>
-                <h2 id="bottom">{bottomtext}</h2>
+                <h2 id="bottom">{text.bottom_text}</h2>
             </div>
         </div>
     )
